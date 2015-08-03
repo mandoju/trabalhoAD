67f737dfd9769148eb84e06e6938a9dd83d4e9ce
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2015-08-03 01:10:39 -0300

    Foi feito uma alteração na hora de lidar com as variáveis poisson. A mudança foi  transformá-la em uma exponencial que diz o tempo até a proxima entrada. tratamento caso o tempo do loop seja menor que 0 foram feito, transformando certos ifs em while

--- main.py
+++ main.py
@@ -6,36 +6,65 @@ def main():
     lambda_entrada = 0.1
     fila = 0
     i = 0
+    j = 0
     servidor = 0
     tempo_proximo = 0
-    entrada_lista =  np.random.poisson(lambda_entrada,100)
-    tempo_proximo_lista = np.random.exponential(1 / lambda_entrada,100)
+    ##entrada_lista =  np.random.poisson(lambda_entrada,100)
+    entrada_lista =  np.random.exponential(1/lambda_entrada,100)
+    tempo_proximo_lista = np.random.exponential(1/lambda_entrada,100)
     print(entrada_lista)
     print(tempo_proximo_lista)
+    fim = 0
 
-    while (i < 100):
-        entrada = entrada_lista[i]
-        fila += entrada
+    entrada = 0
+    tempo_proximo = 0
 
-        if(tempo_proximo == 0 and fila > 0):
-            servidor = 0
+    while (1):
 
+        ##entrada = entrada_lista[i]
+        while(entrada == 0 and i < 100):
+            #fila += entrada
+            ##print('passei aqui')
+            entrada = int(entrada_lista[i])
+            ##print('a entrada ficou' + str(entrada))
+            fila+= 1
+            i += 1
 
-        if (servidor == 0):
-            if (fila > 0):
-                servidor = 1
-                fila -= 1
-                ## checar ser random exponential é a função ideal para o caso
-                tempo_proximo = int(tempo_proximo_lista[i])
-                print('tempo_proximo = ' + str(tempo_proximo))
+        entrada -= 1
 
+        while(tempo_proximo == 0 and fila > 0 and j < 100):
+            servidor = 1
+            fila -= 1
+            tempo_proximo = int(tempo_proximo_lista[j])
+            j += 1
+            ##print('j vale ' + str(j))
+            ##print('tempo_proximo = ' + str(tempo_proximo))
+        if(fila == 0 and tempo_proximo == 0):
+                servidor = 0
         else:
+                tempo_proximo -= 1
 
-            tempo_proximo -= 1
-        print('fila = ' + str(fila))
-        print('servidor = ' + str(servidor))
-        i += 1
+        ##if (servidor == 0):
+        ##   if (fila > 0 and j < 100):
+        ##        servidor = 1
+        ##        fila -= 1
+                ## checar ser random exponential é a função ideal para o caso
+        ##        tempo_proximo = int(tempo_proximo_lista[j])
+        ##        j += 1
+        ##        print('tempo_proximo = ' + str(tempo_proximo))
 
+        ##else:
+
+        ##    tempo_proximo -= 1
+
+
+        if(fim == 0):
+            print('fila = ' + str(fila))
+            print('servidor = ' + str(servidor))
+        else:
+            return
+        if(not(fila > 0 or ( i < 100 and j < 100))):
+            fim = 1
 
 ##executando a função main
 main()
