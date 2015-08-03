028a5b4cebb2516819f70f60ac88d2903e4ad4b6
marcelocanario <marcelocanario@gmail.com>
2015-08-03 16:47:23 -0300

    Alterado para rodar várias simulações e obter a média das médias

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
- # -*- coding: latin-1 -*-
-
 __author__ = 'jorge'
 import numpy as np
 def main():
@@ -7,65 +5,65 @@ def main():
 
     #parametros
     lambda_entrada = 0.1    ##lambda do problema
-    numero_de_aleatorios = 10000 ##representa o número de variáveis aleatórias que irá ser gerado
-
-    fila = 0
-    i = 0
-    j = 0
-    servidor = 0
-    tempo_proximo = 0
-
-    ##variáveis para calcular a média
-    tempo = 0
-    esperanca = 0
-
-    fim = 0
-
+    numero_de_aleatorios = 100 ##representa o número de variáveis aleatórias que irá ser gerado
+    numero_ciclos= 1000
 
     ##TODO: melhores nomes para as variáveis
 
-    entrada = 0  ##tempo até a próxima entrada
-    tempo_proximo = 0  ##tempo até terminar a execução no servidor
+    media=0
+    for q in range(numero_ciclos):
+        fila = 0
+        i = 0
+        j = 0
+        servidor = 0
+        tempo_proximo = 0
+        entrada = 0  ##tempo até a próxima entrada
+        tempo_proximo = 0  ##tempo até terminar a execução no servidor
+        ##variáveis para calcular a média
+        tempo = 0
+        esperanca = 0
+        fim = 0
 
+        while (1):
 
-    while (1):
+            if(i == numero_de_aleatorios and j == numero_de_aleatorios):
+                esperanca = esperanca / tempo
+                #print('a número médio de pessoas no sistema é: ' + str(esperanca))
+                media= media + esperanca/numero_ciclos
+                break
 
-        if(i == numero_de_aleatorios and j == numero_de_aleatorios):
-            esperanca = esperanca / tempo
-            print('a número médio de pessoas no sistema é: ' + str(esperanca))
-            return
+            while(entrada == 0 and i < numero_de_aleatorios):
+                entrada = int(np.random.exponential(1/lambda_entrada)) ##Gera uma nova entrada exponencial
+                fila+= 1
+                i += 1
 
-        while(entrada == 0 and i < numero_de_aleatorios):
-            entrada = int(np.random.exponential(1/lambda_entrada)) ##Gera uma nova entrada exponencial
-            fila+= 1
-            i += 1
+            entrada -= 1
 
-        entrada -= 1
+            ## while nescessário caso caia em um tempo = 0 novamente
+            while(tempo_proximo == 0 and fila > 0 and j < numero_de_aleatorios):
+                servidor = 1
+                fila -= 1
+                tempo_proximo = int(np.random.exponential(1/lambda_entrada)) ##Gera um novo tempo exponencial
+                j += 1
+            if(fila == 0 and tempo_proximo == 0):
+                    servidor = 0
+            else:
+                    tempo_proximo -= 1
 
-        ## while nescessário caso caia em um tempo = 0 novamente
-        while(tempo_proximo == 0 and fila > 0 and j < numero_de_aleatorios):
-            servidor = 1
-            fila -= 1
-            tempo_proximo = int(np.random.exponential(1/lambda_entrada)) ##Gera um novo tempo exponencial
-            j += 1
-        if(fila == 0 and tempo_proximo == 0):
-                servidor = 0
-        else:
-                tempo_proximo -= 1
+            ##coisas inuteis para parar quando já tudo processado
+            ##nunca será executável pois para depois da ultima remessa da entrada
+            if(fim == 0):
+                ##print('fila = ' + str(fila))
+                ##print('servidor = ' + str(servidor))
+                pass
+            else:
+                return
+            if(not(fila > 0 or ( i < numero_de_aleatorios and j < numero_de_aleatorios))):
+                fim = 1
 
-        ##coisas inuteis para parar quando já tudo processado
-        ##nunca será executável pois para depois da ultima remessa da entrada
-        if(fim == 0):
-            ##print('fila = ' + str(fila))
-            ##print('servidor = ' + str(servidor))
-            pass
-        else:
-            return
-        if(not(fila > 0 or ( i < numero_de_aleatorios and j < numero_de_aleatorios))):
-            fim = 1
+            esperanca += fila + servidor ##soma o número de clientes no sistema
+            tempo += 1  ##adiciona mais um no tempo
 
-        esperanca += fila + servidor ##soma o número de clientes no sistema
-        tempo += 1  ##adiciona mais um no tempo
-
-##executando a função main
+    ##executando a função main
+    print("Media das medias: ", media)
 main()
