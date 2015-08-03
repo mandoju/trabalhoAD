a4fbedd3156fb2aae423405c414fd012671c4f44
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2015-08-03 01:33:08 -0300

    Foi feito uma alteração na hora de lidar com as variáveis poisson. A mudança foi  transformá-la em uma exponencial que diz o tempo até a proxima entrada. tratamento caso o tempo do loop seja menor que 0 foram feito, transformando certos ifs em while.
    Melhores comentários
    
    !!! Adicionado a esperança e termina depois do último número aleatório ter sido enviado !!!

--- main.py
+++ main.py
@@ -3,17 +3,29 @@
 __author__ = 'jorge'
 import numpy as np
 def main():
-    lambda_entrada = 0.1
+
+
+    #parametros
+    lambda_entrada = 0.1    ##lambda do problema
+    numero_de_aleatorios = 100 ##representa o número de variáveis aleatórias que irá ser gerado
+
     fila = 0
     i = 0
     j = 0
     servidor = 0
     tempo_proximo = 0
+
+    ##variáveis para calcular a média
+    tempo = 0
+    esperanca = 0
+
     ##entrada_lista =  np.random.poisson(lambda_entrada,100)
-    entrada_lista =  np.random.exponential(1/lambda_entrada,100)
-    tempo_proximo_lista = np.random.exponential(1/lambda_entrada,100)
+
+    entrada_lista =  np.random.exponential(1/lambda_entrada,numero_de_aleatorios)
+    tempo_proximo_lista = np.random.exponential(1/lambda_entrada,numero_de_aleatorios)
     print(entrada_lista)
     print(tempo_proximo_lista)
+
     fim = 0
 
 
@@ -28,7 +40,14 @@ def main():
         ##entrada = entrada_lista[i]
 
          ## while nescessário caso caia em uma entrada = 0 novamente
-        while(entrada == 0 and i < 100):
+
+
+        if(i == numero_de_aleatorios and j == numero_de_aleatorios):
+            esperanca = esperanca / tempo
+            print('a número médio de pessoas no sistema é: ' + str(esperanca))
+            return
+
+        while(entrada == 0 and i < numero_de_aleatorios):
             #fila += entrada
             ##print('passei aqui')
             entrada = int(entrada_lista[i])
@@ -39,7 +58,7 @@ def main():
         entrada -= 1
 
         ## while nescessário caso caia em um tempo = 0 novamente
-        while(tempo_proximo == 0 and fila > 0 and j < 100):
+        while(tempo_proximo == 0 and fila > 0 and j < numero_de_aleatorios):
             servidor = 1
             fila -= 1
             tempo_proximo = int(tempo_proximo_lista[j])
@@ -65,13 +84,19 @@ def main():
         ##    tempo_proximo -= 1
 
 
+
+        ##coisas inuteis para parar quando já tudo processado
+        ##nunca será executável pois para depois da ultima remessa da entrada
         if(fim == 0):
             print('fila = ' + str(fila))
             print('servidor = ' + str(servidor))
         else:
             return
-        if(not(fila > 0 or ( i < 100 and j < 100))):
+        if(not(fila > 0 or ( i < numero_de_aleatorios and j < numero_de_aleatorios))):
             fim = 1
+
+        esperanca += fila + servidor ##soma o número de clientes no sistema
+        tempo += 1  ##adiciona mais um no tempo
 
 ##executando a função main
 main()
