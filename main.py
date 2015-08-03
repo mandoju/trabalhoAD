6abe1609d8b1949e3d8af8c8239b42390e33442e
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2015-08-03 00:21:56 -0300

    Protótipo do inicio do primeiro trabalho :)   -- criando o servidor ... não funcionando ainda   ---  mudando para as variaveis com a lista

--- main.py
+++ main.py
@@ -3,24 +3,37 @@
 __author__ = 'jorge'
 import numpy as np
 def main():
-    lambda_entrada = 5
+    lambda_entrada = 0.1
     fila = 0
+    i = 0
     servidor = 0
     tempo_proximo = 0
+    entrada_lista =  np.random.poisson(lambda_entrada)
+    tempo_proximo_lista = np.random.exponential(1 / lambda_entrada)
 
-    while (1):
-        entrada = np.random.poisson(lambda_entrada)
+
+    while (i < 100):
+        entrada = entrada_lista[i]
         fila += entrada
+
+        if(tempo_proximo == 0 and fila > 0):
+            servidor = 0
+
+
         if (servidor == 0):
             if (fila > 0):
                 servidor = 1
                 fila -= 1
                 ## checar ser random exponential é a função ideal para o caso
-                tempo_proximo = np.random.exponential(1 / lambda_entrada)
+                tempo_proximo = tempo_proximo_lista[i]
+                print('tempo_proximo = ' + str(tempo_proximo))
+
         else:
+
             tempo_proximo -= 1
         print('fila = ' + str(fila))
         print('servidor = ' + str(servidor))
+        i += 1
 
 
 ##executando a função main
