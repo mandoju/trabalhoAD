56420a9f4a3db18dea8ebe8559c6404b87e1ea69
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2015-08-03 21:52:55 -0300

    Implementando o determinístico e uniform

--- main.py
+++ main.py
@@ -77,6 +77,85 @@ def deterministico():
     lambda_tempo = 1/lambda_entrada ##lambda para tempo
     numero_de_aleatorios = 100 ##representa o número de variáveis aleatórias que irá ser gerado
     numero_ciclos= 10000
+    tempo_simulacao = 1000  ##número de loops do while
+
+    media=0
+    for q in range(numero_ciclos):
+        fila = 0
+        i = 0
+        j = 0
+        servidor = 0
+        tempo_proximo = 0
+        entrada = 0  ##tempo até a próxima entrada
+        tempo_proximo = 0  ##tempo até terminar a execução no servidor
+        ##variáveis para calcular a média
+        tempo = 0
+        esperanca = 0
+        fim = 0
+
+        while (1):
+
+
+            if(tempo == tempo_simulacao):
+                esperanca = esperanca / tempo
+                #print('a número médio de pessoas no sistema é: ' + str(esperanca))
+                media= media + esperanca/numero_ciclos
+                break
+            ##if(i == numero_de_aleatorios and j == numero_de_aleatorios):
+            ##    esperanca = esperanca / tempo
+            ##    #print('a número médio de pessoas no sistema é: ' + str(esperanca))
+            ##    media= media + esperanca/numero_ciclos
+            ##    break
+
+            while(entrada == 0 and i < numero_de_aleatorios):
+                entrada = int(lambda_tempo) ##Gera uma nova entrada exponencial
+                fila+= 1
+                i += 1
+
+            entrada -= 1
+
+            ## while nescessário caso caia em um tempo = 0 novamente
+            resto = 0
+            while(tempo_proximo == 0 and fila > 0 and j < numero_de_aleatorios):
+                servidor = 1
+                fila -= 1
+                tempo_proximo = int(1)
+                j += 1
+            if(fila == 0 and tempo_proximo == 0):
+                    servidor = 0
+            else:
+                    tempo_proximo -= 1
+
+            ##coisas inuteis para parar quando já tudo processado
+            ##nunca será executável pois para depois da ultima remessa da entrada
+            if(fim == 0):
+                ##print('fila = ' + str(fila))
+                ##print('servidor = ' + str(servidor))
+                pass
+            else:
+                break
+            if(tempo == tempo_simulacao):
+                fim = 1
+
+            esperanca += fila + servidor ##soma o número de clientes no sistema
+            tempo += 1  ##adiciona mais um no tempo
+
+    ##executando a função main
+    print("Media das medias: ", media)
+
+
+def uniforme():
+
+
+    ##ALERTA!
+    ##NESTE CASO SÓ ALTERA O U DO SERVIDOR
+    ##1, 1.5,2,2.5,...,10
+
+    #parametros
+    u_servidor = 1  ##este parametro que irá variar
+    numero_de_aleatorios = 100 ##representa o número de variáveis aleatórias que irá ser gerado
+    numero_ciclos= 10000
+    tempo_simulacao= 10000 ##número de loops do while
 
     ##TODO: melhores nomes para as variáveis
 
@@ -96,14 +175,19 @@ def deterministico():
 
         while (1):
 
-            if(i == numero_de_aleatorios and j == numero_de_aleatorios):
+            if(tempo == tempo_simulacao):
                 esperanca = esperanca / tempo
                 #print('a número médio de pessoas no sistema é: ' + str(esperanca))
                 media= media + esperanca/numero_ciclos
                 break
+            ##if(i == numero_de_aleatorios and j == numero_de_aleatorios):
+            ##    esperanca = esperanca / tempo
+            ##    #print('a número médio de pessoas no sistema é: ' + str(esperanca))
+            ##    media= media + esperanca/numero_ciclos
+            ##    break
 
             while(entrada == 0 and i < numero_de_aleatorios):
-                entrada = int(lambda_tempo) ##Gera uma nova entrada exponencial
+                entrada = int(np.random.uniform(5,15)) ##Gera uma nova entrada exponencial
                 fila+= 1
                 i += 1
 
@@ -114,7 +198,7 @@ def deterministico():
             while(tempo_proximo == 0 and fila > 0 and j < numero_de_aleatorios):
                 servidor = 1
                 fila -= 1
-                tempo_proximo = int(lambda_tempo)
+                tempo_proximo = int(u_servidor)
                 j += 1
             if(fila == 0 and tempo_proximo == 0):
                     servidor = 0
@@ -128,8 +212,8 @@ def deterministico():
                 ##print('servidor = ' + str(servidor))
                 pass
             else:
-                return
-            if(not(fila > 0 or ( i < numero_de_aleatorios and j < numero_de_aleatorios))):
+                break
+            if(tempo == tempo_simulacao):
                 fim = 1
 
             esperanca += fila + servidor ##soma o número de clientes no sistema
@@ -137,6 +221,7 @@ def deterministico():
 
     ##executando a função main
     print("Media das medias: ", media)
+
 
 
 ##executando a M/M/1
