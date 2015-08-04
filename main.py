fd49f1a6b6d376d6080712c3ecdd7c0084f72753
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2015-08-03 21:08:04 -0300

    Criando a fila determinística na função detereminisitica()

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ def main():
 
 
     #parametros
-    lambda_entrada = 0.1    ##lambda do problema
+    lambda_entrada = 0.05    ##lambda do problema
     numero_de_aleatorios = 100 ##representa o número de variáveis aleatórias que irá ser gerado
-    numero_ciclos= 1000
+    numero_ciclos= 10000
 
     ##TODO: melhores nomes para as variáveis
 
@@ -67,4 +67,79 @@ def main():
 
     ##executando a função main
     print("Media das medias: ", media)
-main()
+
+
+def deterministico():
+
+
+    #parametros
+    lambda_entrada = 0.95    ##lambda do problema
+    lambda_tempo = 1/lambda_entrada ##lambda para tempo
+    numero_de_aleatorios = 100 ##representa o número de variáveis aleatórias que irá ser gerado
+    numero_ciclos= 10000
+
+    ##TODO: melhores nomes para as variáveis
+
+    media=0
+    for q in range(numero_ciclos):
+        fila = 0
+        i = 0
+        j = 0
+        servidor = 0
+        tempo_proximo = 0
+        entrada = 0  ##tempo até a próxima entrada
+        tempo_proximo = 0  ##tempo até terminar a execução no servidor
+        ##variáveis para calcular a média
+        tempo = 0
+        esperanca = 0
+        fim = 0
+
+        while (1):
+
+            if(i == numero_de_aleatorios and j == numero_de_aleatorios):
+                esperanca = esperanca / tempo
+                #print('a número médio de pessoas no sistema é: ' + str(esperanca))
+                media= media + esperanca/numero_ciclos
+                break
+
+            while(entrada == 0 and i < numero_de_aleatorios):
+                entrada = int(lambda_tempo) ##Gera uma nova entrada exponencial
+                fila+= 1
+                i += 1
+
+            entrada -= 1
+
+            ## while nescessário caso caia em um tempo = 0 novamente
+            resto = 0
+            while(tempo_proximo == 0 and fila > 0 and j < numero_de_aleatorios):
+                servidor = 1
+                fila -= 1
+                tempo_proximo = int(lambda_tempo)
+                j += 1
+            if(fila == 0 and tempo_proximo == 0):
+                    servidor = 0
+            else:
+                    tempo_proximo -= 1
+
+            ##coisas inuteis para parar quando já tudo processado
+            ##nunca será executável pois para depois da ultima remessa da entrada
+            if(fim == 0):
+                ##print('fila = ' + str(fila))
+                ##print('servidor = ' + str(servidor))
+                pass
+            else:
+                return
+            if(not(fila > 0 or ( i < numero_de_aleatorios and j < numero_de_aleatorios))):
+                fim = 1
+
+            esperanca += fila + servidor ##soma o número de clientes no sistema
+            tempo += 1  ##adiciona mais um no tempo
+
+    ##executando a função main
+    print("Media das medias: ", media)
+
+
+##executando a M/M/1
+##main()
+##executando a determinística
+deterministico()
