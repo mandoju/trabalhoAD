73f9990f9d80371c31f36459fdcaedb09c5e34a9
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2015-08-03 23:02:42 -0300

    Implementado os cenários 4,5 e 6

--- cenarios_reentrada.py
+++ cenarios_reentrada.py
@@ -1,18 +1,18 @@
 # -*- coding: latin-1 -*-
 __author__ = 'jorge'
 import numpy as np
-def main():
 
-
-    #parametros
-    lambda_entrada = 0.05    ##lambda do problema
-    numero_de_aleatorios = 100 ##representa o número de variáveis aleatórias que irá ser gerado
+def cenario4():
+   #parametros
+    u_servidor = 1    ##U do servidor (tem que variar 1,1,5,2...10
     numero_ciclos= 10000
+    tempo_simulacao = 10000
 
     ##TODO: melhores nomes para as variáveis
 
     media=0
     for q in range(numero_ciclos):
+        lambda_entrada = 0.01  ##lambda fixo
         fila = 0
         i = 0
         j = 0
@@ -27,13 +27,13 @@ def main():
 
         while (1):
 
-            if(i == numero_de_aleatorios and j == numero_de_aleatorios):
+            if(tempo == tempo_simulacao):
                 esperanca = esperanca / tempo
                 #print('a número médio de pessoas no sistema é: ' + str(esperanca))
                 media= media + esperanca/numero_ciclos
                 break
 
-            while(entrada == 0 and i < numero_de_aleatorios):
+            while(entrada == 0):
                 entrada = int(np.random.exponential(1/lambda_entrada)) ##Gera uma nova entrada exponencial
                 fila+= 1
                 i += 1
@@ -41,10 +41,13 @@ def main():
             entrada -= 1
 
             ## while nescessário caso caia em um tempo = 0 novamente
-            while(tempo_proximo == 0 and fila > 0 and j < numero_de_aleatorios):
+            while(tempo_proximo == 0 and fila > 0):
                 servidor = 1
                 fila -= 1
-                tempo_proximo = int(np.random.exponential(1)) ##Gera um novo tempo exponencial
+                aleatorio = np.random.random_sample()
+                if(aleatorio > 0.9):
+                    fila +=1
+                tempo_proximo = int(np.random.exponential(u_servidor)) ##Gera um novo tempo exponencial
                 j += 1
             if(fila == 0 and tempo_proximo == 0):
                     servidor = 0
@@ -53,33 +56,23 @@ def main():
 
             ##coisas inuteis para parar quando já tudo processado
             ##nunca será executável pois para depois da ultima remessa da entrada
-            if(fim == 0):
-                ##print('fila = ' + str(fila))
-                ##print('servidor = ' + str(servidor))
-                pass
-            else:
-                return
-            if(not(fila > 0 or ( i < numero_de_aleatorios and j < numero_de_aleatorios))):
-                fim = 1
-
             esperanca += fila + servidor ##soma o número de clientes no sistema
             tempo += 1  ##adiciona mais um no tempo
 
     ##executando a função main
     print("Media das medias: ", media)
 
-
-def deterministico():
-
-
+def cenario5():
     #parametros
-    lambda_entrada = 0.95    ##lambda do problema
-    lambda_tempo = 1/lambda_entrada ##lambda para tempo
+
+    u_servidor = 1  ##u do servidor precisa variar
     numero_ciclos= 10000
     tempo_simulacao = 1000  ##número de loops do while
 
     media=0
     for q in range(numero_ciclos):
+        lambda_entrada = 0.01   ##lambda fixo do problema
+        lambda_tempo = 1/lambda_entrada ##lambda para tempo
         fila = 0
         i = 0
         j = 0
@@ -118,7 +111,10 @@ def deterministico():
             while(tempo_proximo == 0 and fila > 0):
                 servidor = 1
                 fila -= 1
-                tempo_proximo = int(1)
+                aleatorio = np.random.random_sample()
+                if(aleatorio > 0.9):
+                    fila +=1
+                tempo_proximo = int(1/u_servidor)
                 j += 1
             if(fila == 0 and tempo_proximo == 0):
                     servidor = 0
@@ -142,8 +138,7 @@ def deterministico():
     ##executando a função main
     print("Media das medias: ", media)
 
-
-def uniforme():
+def cenario5():
 
 
     ##ALERTA!
@@ -152,6 +147,7 @@ def uniforme():
 
     #parametros
     u_servidor = 1  ##este parametro que irá variar
+    numero_de_aleatorios = 100 ##representa o número de variáveis aleatórias que irá ser gerado
     numero_ciclos= 10000
     tempo_simulacao= 10000 ##número de loops do while
 
@@ -184,8 +180,8 @@ def uniforme():
             ##    media= media + esperanca/numero_ciclos
             ##    break
 
-            while(entrada == 0):
-                entrada = int(np.random.uniform(5,15)) ##Gera uma nova entrada exponencial
+            while(entrada == 0 and i < numero_de_aleatorios):
+                entrada = int(np.random.uniform(50,150)) ##Gera uma nova entrada exponencial
                 fila+= 1
                 i += 1
 
@@ -193,9 +189,12 @@ def uniforme():
 
             ## while nescessário caso caia em um tempo = 0 novamente
             resto = 0
-            while(tempo_proximo == 0 and fila > 0):
+            while(tempo_proximo == 0 and fila > 0 and j < numero_de_aleatorios):
                 servidor = 1
                 fila -= 1
+                aleatorio = np.random.random_sample()
+                if(aleatorio > 0.9):
+                    fila +=1
                 tempo_proximo = int(u_servidor)
                 j += 1
             if(fila == 0 and tempo_proximo == 0):
@@ -219,8 +218,3 @@ def uniforme():
 
     ##executando a função main
     print("Media das medias: ", media)
-
-##executando a M/M/1
-##main()
-##executando a determinística
-deterministico()
