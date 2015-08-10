c7e3c36c43bd83e6a0a537c5862b53afe408bfaf
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2015-08-10 20:06:33 -0300

    simulações da poisson 2 , 3 e 4

--- simulacoes/poisson2.py
+++ simulacoes/poisson2.py
@@ -6,18 +6,18 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import spline
 
 
-def simula_poisson1():
-    #parametros
-    lambda_entrada = 0.5    ##lambda do problema
-    #numero_de_aleatorios = 10000 ##representa o número de variáveis aleatórias que irá ser gerado
-    tempo_simulacao = 1000
-    numero_ciclos= 1
 
-            ##TODO: melhores nomes para as variáveis
+def simula_poisson2():
+#parametros
+    u_servidor = 1    ##U do servidor (tem que variar 1,1,5,2...10
+    numero_ciclos= 1
+    tempo_simulacao = 1000000
+
+    ##TODO: melhores nomes para as variáveis
 
     media=0
-    tempo_entre_saida_final =  np.array([0] * 100)
     for q in range(numero_ciclos):
+        lambda_entrada = 0.01  ##lambda fixo
         fila = 0
         i = 0
         j = 0
@@ -31,6 +31,7 @@ def simula_poisson1():
         fim = 0
 
         tempo_entre_saida =  []
+        volta = 0
 
         total_saido = 0
         stasis = 0
@@ -38,24 +39,11 @@ def simula_poisson1():
 
         while (1):
 
-            #if(i == numero_de_aleatorios and j == numero_de_aleatorios):
-                #esperanca = esperanca / tempo
-                #print('a número médio de pessoas no sistema é: ' + str(esperanca))
-                #media= media + esperanca/numero_ciclos
-                ##tempo_entre_saida[:] = [x / total_saido for x in tempo_entre_saida]
-                ##print('o tempo entre saida é:')
-                ##print(tempo_entre_saida)
-                ##tempo_entre_saida_final = map(add,tempo_entre_saida_final,tempo_entre_saida)
-                ##tempo_entre_saida_final = tempo_entre_saida_final + np.array(tempo_entre_saida)
-                ##tempo_entre_saida_final = [(x)/numero_ciclos for x in tempo_entre_saida]
-            #    break
             if(tempo == tempo_simulacao):
                 esperanca = esperanca / tempo
                 #print('a número médio de pessoas no sistema é: ' + str(esperanca))
                 media= media + esperanca/numero_ciclos
                 break
-
-
 
             while(entrada == 0):
                 entrada = int(np.random.exponential(1/lambda_entrada)) ##Gera uma nova entrada exponencial
@@ -65,40 +53,54 @@ def simula_poisson1():
             entrada -= 1
 
             ## while nescessário caso caia em um tempo = 0 novamente
+            resto = 0
+
+
             while(tempo_proximo == 0 and fila > 0):
                 servidor = 1
-                fila -= 1
-                ##aqui vamos colocar na lista o tempo que vai demorar
-                total_saido += 1
+                exponencial = np.random.exponential(1/u_servidor)
+                tempo_proximo = int(exponencial)
+                resto += exponencial - tempo_proximo
 
-                exponencial = np.random.exponential(1)
-                tempo_proximo = int(exponencial) ##Gera um novo tempo exponencial
-                tempo_entre_saida.append(stasis + exponencial)
+                if(volta == 0):
+                    fila -=1
+
+                aleatorio = np.random.random_sample()
+                if(aleatorio > 0.1):
+                    volta = 1
+                    ##fila +=1
+                else:
+                    tempo_entre_saida.append(stasis + exponencial)
+                    volta = 0
+
+
+                ##tempo_entre_saida.append(stasis + exponencial)
+
                 stasis = 0
 
+
+                if(resto > 1):
+                    tempo_proximo += 1
                 j += 1
             if(fila == 0 and tempo_proximo == 0):
-                    ##aqui vamos contar o tempo da pessoa que saiu
                     stasis +=1
                     servidor = 0
             else:
                     tempo_proximo -= 1
-
             ##coisas inuteis para parar quando já tudo processado
             ##nunca será executável pois para depois da ultima remessa da entrada
-
-
             esperanca += fila + servidor ##soma o número de clientes no sistema
             tempo += 1  ##adiciona mais um no tempo
 
-    ##executando a função main
-    divisao_ciclos = np.array([numero_ciclos] * 100)
 
-    #print(tempo_entre_saida)
+     #print(tempo_entre_saida)
     total = len(tempo_entre_saida)
-    #print(total)
+    print(total)
     #y_temp = np.array(range(total))
     y = np.array(range(total))/total
+    ##executando a função main
+    #print("Media das medias: ", media)
+
     tempo_entre_saida.sort()
 
     plt.figure(figsize=(8, 6), dpi=100)
--- simulacoes/poisson4.py
+++ simulacoes/poisson4.py
@@ -1,23 +1,25 @@
+__author__ = 'jorge'
 
 # -*- coding: latin-1 -*-
+__author__ = 'jorge'
 
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import spline
 
 
-def simula_poisson1():
-    #parametros
-    lambda_entrada = 0.5    ##lambda do problema
-    #numero_de_aleatorios = 10000 ##representa o número de variáveis aleatórias que irá ser gerado
-    tempo_simulacao = 1000
+def simula_poisson4():
+    print('poisson2')
+   #parametros
+    u_servidor = 1    ##U do servidor (tem que variar 1,1,5,2...10
     numero_ciclos= 1
+    tempo_simulacao = 1000000
 
-            ##TODO: melhores nomes para as variáveis
+    ##TODO: melhores nomes para as variáveis
 
     media=0
-    tempo_entre_saida_final =  np.array([0] * 100)
     for q in range(numero_ciclos):
+        lambda_entrada = 0.01  ##lambda fixo
         fila = 0
         i = 0
         j = 0
@@ -30,7 +32,8 @@ def simula_poisson1():
         esperanca = 0
         fim = 0
 
-        tempo_entre_saida =  []
+        tempo_entre_entrada =  []
+        volta = 0
 
         total_saido = 0
         stasis = 0
@@ -38,77 +41,79 @@ def simula_poisson1():
 
         while (1):
 
-            #if(i == numero_de_aleatorios and j == numero_de_aleatorios):
-                #esperanca = esperanca / tempo
-                #print('a número médio de pessoas no sistema é: ' + str(esperanca))
-                #media= media + esperanca/numero_ciclos
-                ##tempo_entre_saida[:] = [x / total_saido for x in tempo_entre_saida]
-                ##print('o tempo entre saida é:')
-                ##print(tempo_entre_saida)
-                ##tempo_entre_saida_final = map(add,tempo_entre_saida_final,tempo_entre_saida)
-                ##tempo_entre_saida_final = tempo_entre_saida_final + np.array(tempo_entre_saida)
-                ##tempo_entre_saida_final = [(x)/numero_ciclos for x in tempo_entre_saida]
-            #    break
             if(tempo == tempo_simulacao):
                 esperanca = esperanca / tempo
+                tempo_entre_entrada.append(exponencial)
                 #print('a número médio de pessoas no sistema é: ' + str(esperanca))
                 media= media + esperanca/numero_ciclos
                 break
 
-
-
             while(entrada == 0):
-                entrada = int(np.random.exponential(1/lambda_entrada)) ##Gera uma nova entrada exponencial
+                exponencial = np.random.exponential(1/lambda_entrada)
+                entrada = int(exponencial) ##Gera uma nova entrada exponencial
                 fila+= 1
                 i += 1
 
             entrada -= 1
 
             ## while nescessário caso caia em um tempo = 0 novamente
+            resto = 0
+
+
             while(tempo_proximo == 0 and fila > 0):
                 servidor = 1
-                fila -= 1
-                ##aqui vamos colocar na lista o tempo que vai demorar
-                total_saido += 1
+                exponencial = np.random.exponential(1/u_servidor)
+                tempo_proximo = int(exponencial)
+                resto += exponencial - tempo_proximo
 
-                exponencial = np.random.exponential(1)
-                tempo_proximo = int(exponencial) ##Gera um novo tempo exponencial
-                tempo_entre_saida.append(stasis + exponencial)
+                if(volta == 0):
+                    fila -=1
+
+                aleatorio = np.random.random_sample()
+                if(aleatorio > 0.1):
+                    volta = 1
+                    tempo_entre_entrada.append(exponencial)
+                    ##fila +=1
+                else:
+                    ##tempo_entre_saida.append(stasis + exponencial)
+                    volta = 0
+
+
+
+
                 stasis = 0
 
+
+                if(resto > 1):
+                    tempo_proximo += 1
                 j += 1
             if(fila == 0 and tempo_proximo == 0):
-                    ##aqui vamos contar o tempo da pessoa que saiu
                     stasis +=1
                     servidor = 0
             else:
                     tempo_proximo -= 1
-
             ##coisas inuteis para parar quando já tudo processado
             ##nunca será executável pois para depois da ultima remessa da entrada
-
-
             esperanca += fila + servidor ##soma o número de clientes no sistema
             tempo += 1  ##adiciona mais um no tempo
-
-    ##executando a função main
-    divisao_ciclos = np.array([numero_ciclos] * 100)
-
     #print(tempo_entre_saida)
-    total = len(tempo_entre_saida)
-    #print(total)
+    total = len(tempo_entre_entrada)
+    print(total)
     #y_temp = np.array(range(total))
     y = np.array(range(total))/total
-    tempo_entre_saida.sort()
+    ##executando a função main
+    #print("Media das medias: ", media)
+
+    tempo_entre_entrada.sort()
 
     plt.figure(figsize=(8, 6), dpi=100)
 
-    x_smooth = np.linspace(min(tempo_entre_saida), max(tempo_entre_saida), 1000)
-    y_smooth = spline (tempo_entre_saida, y, x_smooth)
+    x_smooth = np.linspace(min(tempo_entre_entrada), max(tempo_entre_entrada), 1000)
+    y_smooth = spline (tempo_entre_entrada, y, x_smooth)
     plt.plot(x_smooth, y_smooth,'-', label="Curva Amortizada")
     #plt.plot(xp, yp, 'bo', label="Pontos Da Curva")
 
-    plt.axis([0, 1.1*(max(tempo_entre_saida)), 0, 1.2*(max(y))])
+    plt.axis([0, 1.1*(max(tempo_entre_entrada)), 0, 1.2*(max(y))])
     plt.suptitle('Poisson1', fontsize=20)
     plt.xlabel('tempo_entre_saidas', fontsize=15)
     plt.ylabel('Y', fontsize=15)
