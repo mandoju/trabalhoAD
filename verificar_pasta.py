53ab6e5a2911400f6ddaa0ef08f8ab49d57205e7
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2015-08-10 18:47:53 -0300

    Modificações para o tentar calcular o tempo vazio

--- verificar_pasta.py
+++ verificar_pasta.py
@@ -122,4 +122,94 @@ def pasta1():
 
     return tempo_vazio,tempo_entre_entrada_vazio,total_vazio
 
-print(pasta1())
+def pasta2():
+   #parametros
+    u_servidor = 1    ##U do servidor (tem que variar 1,1,5,2...10
+    numero_ciclos= 1
+    tempo_simulacao = 1000000
+
+    ##TODO: melhores nomes para as variáveis
+
+    media=0
+    for q in range(numero_ciclos):
+        lambda_entrada = 0.01  ##lambda fixo
+        fila = 0
+        i = 0
+        j = 0
+        servidor = 0
+        tempo_proximo = 0
+        entrada = 0  ##tempo até a próxima entrada
+        tempo_proximo = 0  ##tempo até terminar a execução no servidor
+        ##variáveis para calcular a média
+        tempo = 0
+        esperanca = 0
+        fim = 0
+        print(q)
+
+        tempo_entre_entrada_vazio =  0
+        tempo_vazio = 0
+
+        total_vazio = 0
+
+        while (1):
+
+            if(tempo == tempo_simulacao):
+                esperanca = esperanca / tempo
+                #print('a número médio de pessoas no sistema é: ' + str(esperanca))
+                media= media + esperanca/numero_ciclos
+                break
+
+            while(entrada == 0):
+
+                if(servidor == 0 ):
+                    tempo_entre_entrada_vazio +=  exponencial_entrada
+                    total_vazio += 1
+
+                exponencial_entrada = np.random.exponential(1/lambda_entrada)
+                entrada = int(exponencial_entrada) ##Gera uma nova entrada exponencial
+                fila+= 1
+                i += 1
+
+            entrada -= 1
+
+            ## while nescessário caso caia em um tempo = 0 novamente
+            resto = 0
+
+
+            while(tempo_proximo == 0 and fila > 0):
+                servidor = 1
+
+                fila -= 1
+
+                if(volta == 1):
+                    fila +=1
+                    if(servidor == 0):
+                        tempo_entre_entrada_vazio +=  exponencial_entrada
+                        total_vazio += 1
+
+
+
+                aleatorio = np.random.random_sample()
+                if(aleatorio > 0.1):
+                    volta = 1
+                else:
+                    volta = 0
+
+                exponencial_saida = np.random.exponential(1/u_servidor)
+                tempo_proximo = int(exponencial_saida)
+                resto += exponencial_saida - tempo_proximo
+                if(resto > 1):
+                    tempo_proximo += 1
+                j += 1
+            if(fila == 0 and tempo_proximo == 0):
+                    tempo_vazio += 1
+                    servidor = 0
+            else:
+                    tempo_proximo -= 1
+            ##coisas inuteis para parar quando já tudo processado
+            ##nunca será executável pois para depois da ultima remessa da entrada
+            esperanca += fila + servidor ##soma o número de clientes no sistema
+            tempo += 1  ##adiciona mais um no tempo
+
+    ##executando a função main
+    print("Media das medias: ", media)
