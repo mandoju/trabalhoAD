3dcdb00a33b14429f79a9d2de3ffa8502f422495
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2015-08-19 21:04:02 -0300

    apagado o pasta 2

--- verificar_pasta.py
+++ verificar_pasta.py
@@ -127,7 +127,7 @@ def pasta1():
 
     return tempo_vazio,tempo_entre_entrada_vazio,total_vazio
 
-def pasta2_correto():
+def pasta2():
 
     #parametros
     u_servidor = 1    ##U do servidor (tem que variar 1,1,5,2...10
@@ -285,128 +285,7 @@ def pasta2_correto():
 
     return fracao_vazio,fracao_entrada,total_vazio
 
-
-def pasta2():
-   #parametros
-    u_servidor = 1    ##U do servidor (tem que variar 1,1,5,2...10
-    numero_ciclos= 1
-    tempo_simulacao = 1000000
-
-    ##TODO: melhores nomes para as variáveis
-
-    media=0
-    for q in range(numero_ciclos):
-        lambda_entrada = 0.01  ##lambda fixo
-        fila = 0
-        i = 0
-        j = 0
-        servidor = 0
-        tempo_proximo = 0
-        entrada = 0  ##tempo até a próxima entrada
-        tempo_proximo = 0  ##tempo até terminar a execução no servidor
-        ##variáveis para calcular a média
-        tempo = 0
-        esperanca = 0
-        fim = 0
-        print(q)
-
-        tempo_entre_entrada_vazio =  0
-        tempo_vazio = 0
-
-        total_vazio = 0
-
-        total_entrada = 0
-        total_saido = 0
-
-        ultima_entrada = 0
-
-        exponencial_entrada = 0
-        exponencial_saida = 0
-
-        aleatorio = np.random.random_sample()
-        if(aleatorio > 0.1):
-             volta = 1
-        else:
-             volta = 0
-
-        while (1):
-
-            if(tempo == tempo_simulacao):
-                esperanca = esperanca / tempo
-                #print('a número médio de pessoas no sistema é: ' + str(esperanca))
-                media= media + esperanca/numero_ciclos
-                break
-
-            while(entrada == 0):
-
-                if(servidor == 0 ):
-                    tempo_entre_entrada_vazio +=  exponencial_entrada
-                    total_vazio += tempo - ultima_entrada
-
-                exponencial_entrada = np.random.exponential(1/lambda_entrada)
-                entrada = int(exponencial_entrada) ##Gera uma nova entrada exponencial
-                fila+= 1
-                ultima_entrada = tempo
-                i += 1
-
-            entrada -= 1
-
-            ## while nescessário caso caia em um tempo = 0 novamente
-            resto = 0
-
-
-            while(tempo_proximo == 0 and fila > 0):
-                servidor = 1
-
-                fila -= 1
-
-                if(volta == 1):
-                    if(servidor == 0):
-                        tempo_entre_entrada_vazio +=  exponencial_entrada
-                        total_vazio += tempo - ultima_entrada
-                        total_entrada += 1
-                    fila += 1
-                    ultima_entrada = tempo
-                else:
-                    total_saido +=1
-
-
-                aleatorio = np.random.random_sample()
-                if(aleatorio > 0.1):
-                    volta = 1
-                else:
-                    volta = 0
-
-                exponencial_saida = np.random.exponential(1/u_servidor)
-                tempo_proximo = int(exponencial_saida)
-                resto += exponencial_saida - tempo_proximo
-                if(resto > 1):
-                    tempo_proximo += 1
-                j += 1
-            if(fila == 0 and tempo_proximo == 0):
-                    tempo_vazio += 1
-                    servidor = 0
-            else:
-                    tempo_proximo -= 1
-            ##coisas inuteis para parar quando já tudo processado
-            ##nunca será executável pois para depois da ultima remessa da entrada
-            esperanca += fila + servidor ##soma o número de clientes no sistema
-            tempo += 1  ##adiciona mais um no tempo
-
-    ##executando a função main
-    ##print("Media das medias: ", media)
-
-    tempo_vazio = tempo_vazio / tempo_simulacao
-    tempo_entre_entrada_vazio = tempo_entre_entrada_vazio / tempo_simulacao
-    total_vazio /= total_saido
-
-    print(tempo_vazio)
-    print(tempo_entre_entrada_vazio)
-    print(total_vazio)
-
-    return tempo_vazio,tempo_entre_entrada_vazio,total_vazio
-
-def pasta_3():
+def pasta3():
     #parametros
     u_servidor = 1    ##U do servidor (tem que variar 1,1,5,2...10
     numero_ciclos= 1
